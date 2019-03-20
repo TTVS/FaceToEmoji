15c6b89cadfe8acd9f61dd2e250e23e5fb36ecd0
Therrense Lua <terranzdev@gmail.com>
2019-03-20 19:38:49 +1100

    Initial Commit

---
+++ FaceToEmoji/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceToEmoji
+//
+//  Created by Terranz on 20/3/19.
+//  Copyright © 2019 terradevteam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
